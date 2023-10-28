8e30027384a9c1704cb8c49dd45f00630c7b74a1
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2023-10-27 18:45:12 -0600

    [FluentTask] - Zip3 - TT

--- FluentTask/Sources/FluentTask/Workers/Zip.swift
+++ FluentTask/Sources/FluentTask/Workers/Zip.swift
@@ -31,6 +31,32 @@ extension Workers {
             }
         }
     }
+    
+    struct Zip3<Success: Sendable, Failure: Error>: AsynchronousUnitOfWork {
+        let taskCreator: @Sendable () -> Task<Success, Failure>
+        
+        init<U: AsynchronousUnitOfWork, D0: AsynchronousUnitOfWork, D1: AsynchronousUnitOfWork>(priority: TaskPriority?, upstream: U, d0: D0, d1: D1) where U.Failure == D0.Failure, D0.Failure == D1.Failure, Failure == U.Failure, Success == (U.Success, D0.Success, D1.Success), Failure == Error {
+            taskCreator = {
+                Task(priority: priority) {
+                    async let u = try await upstream.taskCreator().value
+                    async let d_0 = try await d0.taskCreator().value
+                    async let d_1 = try await d1.taskCreator().value
+                    return (try await u, try await d_0, try await d_1)
+                }
+            }
+        }
+
+        init<U: AsynchronousUnitOfWork, D0: AsynchronousUnitOfWork, D1: AsynchronousUnitOfWork>(priority: TaskPriority?, upstream: U, d0: D0, d1: D1) where U.Failure == D0.Failure, D0.Failure == D1.Failure, Failure == U.Failure, Success == (U.Success, D0.Success, D1.Success), Failure == Never {
+            taskCreator = {
+                Task(priority: priority) {
+                    async let u = await upstream.taskCreator().value
+                    async let d_0 = await d0.taskCreator().value
+                    async let d_1 = await d1.taskCreator().value
+                    return (await u, await d_0, await d_1)
+                }
+            }
+        }
+    }
 }
 
 extension AsynchronousUnitOfWork {
@@ -48,5 +74,22 @@ extension AsynchronousUnitOfWork {
     
     public func zip<D: AsynchronousUnitOfWork, T: Sendable>(priority: TaskPriority? = nil, _ downstream: D, @_inheritActorContext @_implicitSelfCapture transform: @escaping @Sendable ((Success, D.Success)) async -> T) -> some AsynchronousUnitOfWork<T, Error> where Failure == D.Failure, Failure == Never {
         Workers.Map<T>(priority: priority, upstream: Workers.Zip<(Success, D.Success), Never>(priority: priority, upstream: self, downstream: downstream), transform: transform)
+    }
+    
+    // zip3
+    public func zip<D0: AsynchronousUnitOfWork, D1: AsynchronousUnitOfWork>(priority: TaskPriority? = nil, _ d0: D0, _ d1: D1) -> some AsynchronousUnitOfWork<(Success, D0.Success, D1.Success), Failure> where Failure == D0.Failure, D0.Failure == D1.Failure, Failure == Error {
+        Workers.Zip3(priority: priority, upstream: self, d0: d0, d1: d1)
+    }
+    
+    public func zip<D0: AsynchronousUnitOfWork, D1: AsynchronousUnitOfWork>(priority: TaskPriority? = nil, _ d0: D0, _ d1: D1) -> some AsynchronousUnitOfWork<(Success, D0.Success, D1.Success), Failure> where Failure == D0.Failure, D0.Failure == D1.Failure, Failure == Never {
+        Workers.Zip3(priority: priority, upstream: self, d0: d0, d1: d1)
+    }
+    
+    public func zip<D0: AsynchronousUnitOfWork, D1: AsynchronousUnitOfWork, T: Sendable>(priority: TaskPriority? = nil, _ d0: D0, _ d1: D1, @_inheritActorContext @_implicitSelfCapture transform: @escaping @Sendable ((Success, D0.Success, D1.Success)) async throws -> T) -> some AsynchronousUnitOfWork<T, Error> where Failure == D0.Failure, D0.Failure == D1.Failure, Failure == Error {
+        Workers.TryMap<T>(priority: priority, upstream: Workers.Zip3<(Success, D0.Success, D1.Success), Error>(priority: priority, upstream: self, d0: d0, d1: d1), transform: transform)
+    }
+    
+    public func zip<D0: AsynchronousUnitOfWork, D1: AsynchronousUnitOfWork, T: Sendable>(priority: TaskPriority? = nil, _ d0: D0, _ d1: D1, @_inheritActorContext @_implicitSelfCapture transform: @escaping @Sendable ((Success, D0.Success, D1.Success)) async -> T) -> some AsynchronousUnitOfWork<T, Error> where Failure == D0.Failure, D0.Failure == D1.Failure, Failure == Never {
+        Workers.Map<T>(priority: priority, upstream: Workers.Zip3<(Success, D0.Success, D1.Success), Never>(priority: priority, upstream: self, d0: d0, d1: d1), transform: transform)
     }
 }
--- FluentTask/Tests/FluentTaskTests/WorkerTests/ZipTests.swift
+++ FluentTask/Tests/FluentTaskTests/WorkerTests/ZipTests.swift
@@ -41,10 +41,54 @@ final class ZipTests: XCTestCase {
     
     func testZipTransformCombinesTasks() async throws {
         let t1 = DeferredTask { 1 }
-        
+    
         let t2 = DeferredTask { "A" }
         let t3 = t2.zip(t1) { $0 + String(describing: $1) }
         let val = try await t3.result.get() // Steak sauce!!!
         XCTAssertEqual(val, "A1")
+    }
+    
+    func testZip3CombinesTasks_WithExplicitFailure() async throws {
+        let t1 = DeferredTask<Int, Error> { 1 }
+        let t2 = DeferredTask<String, Error> { "A" }
+        let t3 = DeferredTask<Bool, Error> { true }
+        
+        let t4 = t2.zip(t1, t3)
+        let val = try await t4.result.get() // Steak sauce!!!
+        XCTAssertEqual(val.0, "A")
+        XCTAssertEqual(val.1, 1)
+        XCTAssertEqual(val.2, true)
+    }
+    
+    func testZip3CombinesTasks() async throws {
+        let t1 = DeferredTask { 1 }
+        let t2 = DeferredTask { "A" }
+        let t3 = DeferredTask { true }
+
+        let t4 = t2.zip(t1, t3)
+        let val = try await t4.result.get() // Steak sauce!!!
+        XCTAssertEqual(val.0, "A")
+        XCTAssertEqual(val.1, 1)
+        XCTAssertEqual(val.2, true)
+    }
+    
+    func testZip3TransformCombinesTasks_WithExplicitFailure() async throws {
+        let t1 = DeferredTask<Int, Error> { 1 }
+        let t2 = DeferredTask<String, Error> { "A" }
+        let t3 = DeferredTask<Bool, Error> { true }
+        
+        let t4 = t2.zip(t1, t3) { $0 + String(describing: $1) + String(describing: $2) }
+        let val = try await t4.result.get() // Steak sauce!!!
+        XCTAssertEqual(val, "A1true")
+    }
+    
+    func testZip3TransformCombinesTasks() async throws {
+        let t1 = DeferredTask { 1 }
+        let t2 = DeferredTask { "A" }
+        let t3 = DeferredTask { true }
+        
+        let t4 = t2.zip(t1, t3) { $0 + String(describing: $1) + String(describing: $2) }
+        let val = try await t4.result.get() // Steak sauce!!!
+        XCTAssertEqual(val, "A1true")
     }
 }
