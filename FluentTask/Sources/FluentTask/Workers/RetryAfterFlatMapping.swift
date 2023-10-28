5773289661e5f6100118c08edc72488f50c8b2e9
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2023-10-28 16:26:53 -0600

    [FluentTask] - RetryOnAfterFlatMapping - TT

--- FluentTask/Sources/FluentTask/Workers/RetryAfterFlatMapping.swift
+++ FluentTask/Sources/FluentTask/Workers/RetryAfterFlatMapping.swift
@@ -26,10 +26,36 @@ extension Workers {
             }
         }
     }
+    
+    struct RetryOnAfterFlatMapping<Success: Sendable>: AsynchronousUnitOfWork {
+        let state: TaskState<Success>
+
+        init<U: AsynchronousUnitOfWork, D: AsynchronousUnitOfWork, E: Error & Equatable>(upstream: U, retries: UInt, error: E, @_inheritActorContext @_implicitSelfCapture transform: @escaping @Sendable (Error) async throws -> D) where U.Success == Success {
+            guard retries > 0 else { state = upstream.state; return }
+            state = TaskState {
+                for _ in 0..<retries {
+                    do {
+                        return try await upstream.operation()
+                    } catch(let err) {
+                        guard let unwrappedError = (err as? E),
+                              unwrappedError == error else { throw err }
+                        _ = try await transform(error).operation()
+                        continue
+                    }
+                }
+                
+                return try await upstream.operation()
+            }
+        }
+    }
 }
 
 extension AsynchronousUnitOfWork {
     public func retry<D: AsynchronousUnitOfWork>(_ retries: UInt = 1, @_inheritActorContext @_implicitSelfCapture _ transform: @escaping @Sendable (Error) async throws -> D) -> some AsynchronousUnitOfWork<Success> {
         Workers.RetryAfterFlatMapping(upstream: self, retries: retries, transform: transform)
+    }
+    
+    public func retry<D: AsynchronousUnitOfWork, E: Error & Equatable>(_ retries: UInt = 1, on error: E, @_inheritActorContext @_implicitSelfCapture _ transform: @escaping @Sendable (Error) async throws -> D) -> some AsynchronousUnitOfWork<Success> {
+        Workers.RetryOnAfterFlatMapping(upstream: self, retries: retries, error: error, transform: transform)
     }
 }
